55755b0a539f48138328cb633e741ecdfab38bfa
Joshua <pointtech1924@gmail.com>
2023-10-09 23:44:52 +0530

    Update

---
+++ Linguistic/src/Language/ConnectionClass.java
@@ -0,0 +1,35 @@
+package Language;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionClass {
+    Connection con;
+    Statement stm;
+
+    ConnectionClass() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Language", "root", "joshua123");
+            stm = con.createStatement();
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ConnectionClass();
+    }
+
+    Object getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
